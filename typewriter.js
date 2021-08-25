113059bf8a44b7efa4360ec0db91645fe4fe6eb0
Wahab Alamin <wahabalamin@gmail.com>
2021-08-25 12:41:16 -0400

    updated file to use a for see loop instead of for of as it is easier to implement the new line at the end

--- typewriter.js
+++ typewriter.js
@@ -2,9 +2,21 @@ const sentence = "hello there from lighthouse labs";
 
 let time = 50;
 
-for (const char of sentence) {
+// for (const char of sentence) {
+//   time += 50;
+//   setTimeout(() => {
+//     process.stdout.write(char);
+//     process.stdout.write('\n');
+
+//   }, time);
+// }
+
+for (let i = 0; i < sentence.length; i ++) {
   time += 50;
   setTimeout(() => {
-    process.stdout.write(char);
+    process.stdout.write(sentence[i]);
+    if(i === sentence.length - 1) {
+      process.stdout.write('\n'); 
+    }
   }, time);
 }
